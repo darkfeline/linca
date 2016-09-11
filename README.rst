55e2b2ff7f7df9ce23a8f5016cdad5ab696c3c4b
Allen Li <darkfeline@felesatra.moe>
2016-09-11 00:54:53 -0700

    Repackage

--- README.rst
+++ README.rst
@@ -3,13 +3,13 @@ linca
 
 linca is a simple directory watching and linking program.
 
-Run linca with arguments supplying watch and destination directories:
+Run linca with arguments supplying watch and destination directories::
 
     $ linca a b
 
-All files and directories created in or moved to `a` will be linked to `b`.
+All files and directories created in or moved to ``a`` will be linked to ``b``.
 Specifically, on inotify events CREATE and MOVED_TO, linca will link files and
-`cp -al` directories to `b`.
+``cp -al`` directories to ``b``.
 
 linca is useful for, e.g., processing downloaded files from torrent clients.
 
--- setup.py
+++ setup.py
@@ -1,20 +1,27 @@
 #!/usr/bin/env python3
 
-from setuptools import find_packages, setup
+from setuptools import setup
 
 setup(
-    name='linca',
+    name='mir.linca',
     version='0.1.0',
-    packages=find_packages(),
-    entry_points={
-        'console_scripts': [
-            'linca = linca:main',
-        ],
-    },
-
+    description='Link new files in watched directory elsewhere.',
+    long_description='',
+    keywords='',
+    url='https://github.com/darkfeline/mir.linca',
     author='Allen Li',
     author_email='darkfeline@felesatra.moe',
-    description='',
-    license='',
-    url='',
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Console',
+        'Intended Audience :: End Users/Desktop',
+        'Programming Language :: Python :: 3.5',
+    ],
+
+    py_modules=['mir.linca'],
+    entry_points={
+        'console_scripts': [
+            'linca = mir.linca:main',
+        ],
+    },
 )
